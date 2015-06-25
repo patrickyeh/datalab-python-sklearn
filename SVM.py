4f116a2841ce0cf1df47b161ae65e898d87c667d
PatrickYeh <patrickyeh@iii.org.tw>
2015-06-25 10:19:24 +0800

    First Commit

---
+++ SVM.py
@@ -0,0 +1,41 @@
+__author__ = 'Vetom'
+from datalab.Model import ClassificationModel
+import numpy as np
+from sklearn import svm
+from datalab.DataTransform import DataTransform
+
+class SVMModel(ClassificationModel):
+    def __init__(self):
+        self.model = None
+
+    def train(self,input_path,**param):
+        obj_dt = DataTransform(input_path,param)
+        obj_dt.scan_data_type()
+        x,y = obj_dt.fetch_data()
+        clf = svm.SVC()
+        model = clf.fit(x,y)
+        self.model = model
+
+        return model
+
+    def predict_by_file(self,input_path,**param):
+        x,y = self._fetch_file(input_path,**param)
+        return self.predict_by_object(x)
+
+    def validate_by_file(self,input_path,**param):
+        x,y = self._fetch_file(input_path,**param)
+        return self.validate_by_object(x,y)
+
+    def predict_by_object(self,obj_array):
+        return self.model.predict(obj_array)
+
+    def validate_by_object(self,obj_test_x,obj_test_y):
+        return self.model.score(obj_test_x,obj_test_y)
+
+
+
+
+if __name__ == '__main__':
+    obj_model = SVMModel()
+    obj_model.train('C:/data/a3a',n_features=123)
+    print obj_model.validate_by_file('C:/data/a3a.t',n_features=123)
